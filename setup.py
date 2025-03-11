397be80d44af188101ccafdd9d59dc2f2fb898e7
Abyot <89458721+abyot2021@users.noreply.github.com>
2025-03-11 15:28:13 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with io.open("README.rst", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ssam",
-    version="1.1.2",
-    author="Jeongbin Park",
-    author_email="jeongbin.park@pusan.ac.kr",
-    description="SSAM",
+    name="VarCas-OFFinder",
+    version="0.1.0",
+    author="Abyot Melkamu",
+    author_email="abyot@pusan.ac.kr",
+    description="VarCas-OFFinder",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pnucolab/ssam",
+    url="https://github.com/pnucolab/rgen-toolkit",
     packages=setuptools.find_packages(),
 )
